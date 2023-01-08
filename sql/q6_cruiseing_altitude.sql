a05b8b6651830a85d51e5068c87a44f93472d969
0102-yang <1657375983@qq.com>
2023-01-08 09:26:43 +0800

    Add output of q6.

--- sql/q6_cruiseing_altitude.sql
+++ sql/q6_cruiseing_altitude.sql
@@ -7,6 +7,17 @@ where t.title_id in (
         where (name like "%Cruise%" or name like "%cruise%")
         and born = "1962"
     )
- )
- order by votes desc
- limit 10;
+)
+order by votes desc
+limit 10;
+
+/* Oblivion|520383
+Mission: Impossible|423228
+Top Gun|408389
+Magnolia|311030
+Born on the Fourth of July|106667
+Days of Thunder|88698
+Lions for Lambs|50257
+Without Limits|7127
+Space Station 3D|1693
+Nickelodeon Kids' Choice Awards 2012|212 */
